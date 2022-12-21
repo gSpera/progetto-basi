0695fbc0ae06b85074c1348390fd71d42dea6978
Giovanni Spera <giovanni.spera2011@libero.it>
2022-12-21 10:24:27 +0100

    Added procedures

--- init.sql
+++ init.sql
@@ -54,12 +54,44 @@ CREATE TABLE stato (
     stato integer,
     quando date
 );
+ALTER TABLE utente ADD CONSTRAINT fk_utente_azienda
+FOREIGN KEY(azienda_id) REFERENCES azienda(id);
+
+ALTER TABLE ordine ADD CONSTRAINT fk_ordine_produttore
+FOREIGN KEY(produttore_id) REFERENCES azienda(id);
+
+ALTER TABLE ordine ADD CONSTRAINT fk_ordine_destinatario
+FOREIGN KEY(destinatario_id) REFERENCES azienda(id);
+
+ALTER TABLE viaggio ADD CONSTRAINT fk_viaggio_ordine
+FOREIGN KEY(id_ordine) REFERENCES ordine(id);
+
+ALTER TABLE stato ADD CONSTRAINT fk_stato_ordine
+FOREIGN KEY(ordine_id) REFERENCES ordine(id);
+
+CREATE OR REPLACE PROCEDURE aggiungi_utente(nome IN utente.nome%type, password IN varchar, azienda_nome IN azienda.nome%type)
+IS
+    hash utente.password%type;
+    azienda_id azienda.id%type;
+BEGIN
+    SELECT STANDARD_HASH(password, 'SHA256') INTO hash FROM dual;
+    SELECT id INTO azienda_id FROM azienda WHERE nome=azienda_nome;
+    INSERT INTO utente VALUES (nome, hash, azienda_id);
+END;
+
+CREATE OR REPLACE PROCEDURE cambia_password_utente(utente_nome IN utente.nome%type, nuova_password IN varchar)
+IS
+    hash utente.password%type;
+BEGIN
+    SELECT STANDARD_HASH(nuova_password, 'SHA256') INTO hash FROM dual;
+    UPDATE utente SET utente.password=hash WHERE utente.nome=utente_nome;
+END;
 
 -- Trigger 
 CREATE TRIGGER order_new AFTER INSERT ON ordine FOR EACH ROW
     INSERT INTO stato VALUES (stato_seq.nextval, :new.id, 0, CURRENT_TIMESTAMP)
 
-CREATE TRIGGER ultimi_stati_update AFTER INSERT ON stato
+CREATE TRIGGER ultimi_stati_update AFTER INSERT OR UPDATE OR DELETE ON stato
 BEGIN
     CREATE OR REPLACE MATERIALIZED VIEW ultimi_stati AS
     SELECT ordine.ddt, produttore.nome as produttore_nome, destinatario.nome as destinatario_nome, ordine.num_colli, ordine.ritirare_assegno, MAX(stato.stato) as stato, stato_string.value as stato_string, MAX(stato.quando) as quando
