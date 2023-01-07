9279aae97baece840e58bb6cf6bf64c00f4ad893
Giovanni Spera <giovanni.spera2011@libero.it>
2023-01-07 17:53:52 +0100

    Fix some bugs in init_sqlite.sql

--- init_sqlite.sql
+++ init_sqlite.sql
@@ -8,11 +8,11 @@ CREATE TABLE azienda (
     id integer primary key,
     ruolo integer, -- 0: Spera Logistica, 1: Produttore, 2: Destinatario
     nome varchar(20) not null,
-    indirizzo varchar(50) not null,
+    indirizzo varchar(50),
     comune varchar(20),
     regione integer not null,
     partita_iva char(11),
-    codice_univoco char(6),
+    codice_univoco char(6)
 );
 
 CREATE TABLE ordine (
@@ -91,7 +91,7 @@ CREATE TRIGGER order_new AFTER INSERT ON ordine FOR EACH ROW
     BEGIN INSERT INTO stato VALUES (NULL, NEW.id, 0, strftime("%Y-%m-%d %H:%M:%S")); END;
 
 CREATE VIEW ultimi_stati AS
-SELECT ordine.id,ordine.id,  ordine.ddt, produttore.id as produttore_id, destinatario.id as destinatario_id, produttore.nome as produttore_nome, destinatario.nome as destinatario_nome, ordine.num_colli, ordine.ritirare_assegno, MAX(stato.stato) as stato, stato_string.value as stato_string, MAX(stato.quando) as quando
+SELECT ordine.id, ordine.ddt, produttore.id as produttore_id, destinatario.id as destinatario_id, produttore.nome as produttore_nome, destinatario.nome as destinatario_nome, ordine.num_colli, ordine.ritirare_assegno, MAX(stato.stato) as stato, stato_string.value as stato_string, MAX(stato.quando) as quando
 FROM ordine
  JOIN stato ON ordine.id = stato.ordine_id
  JOIN azienda produttore ON ordine.produttore_id = produttore.id
@@ -100,5 +100,5 @@ FROM ordine
 GROUP BY ordine.ddt, produttore.nome, destinatario.nome, ordine.num_colli, ordine.ritirare_assegno, stato_string.value
 ORDER BY quando DESC;
 
-INSERT INTO azienda VALUES (-1, 0, 'Spera Logistica', 'Via Speranzosa 123', '12332112312', 'A1A2A3');
+INSERT INTO azienda VALUES (-1, 0, 'Spera Logistica', 'Via Speranzosa 123', NULL, 3, '12332112312', 'A1A2A3');
 INSERT INTO utente VALUES ('gs', '7a5443b6636713baa6350c1cf3ec620b2771a8d411ea3180c5d46b502b9ab77d', -1);
